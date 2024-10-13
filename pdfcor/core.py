763420111d287c45c6a14d8de53a52f5383d6503
infocornouaille <90894019+infocornouaille@users.noreply.github.com>
2024-10-13 08:17:00 +0200

    Update core.py

--- pdfcor/core.py
+++ pdfcor/core.py
@@ -70,3 +70,62 @@ def process_folder(folder_path, output_dir, recursive=False, resize=False):
             if file.lower().endswith(".pdf"):
                 pdf_path = os.path.join(folder_path, file)
                 process_pdf(pdf_path, output_dir, resize)
+
+
+def merge_pdfs(input_folder, output_file=None):
+    """
+    Fusionne tous les PDF d'un dossier en un seul fichier.
+
+    :param input_folder: Chemin du dossier contenant les PDF à fusionner
+    :param output_file: Nom du fichier de sortie (optionnel)
+    """
+    input_folder = os.path.abspath(input_folder)
+
+    if output_file is None:
+        folder_name = os.path.basename(input_folder)
+        if folder_name == "":  # Cas où input_folder est "."
+            folder_name = os.path.basename(os.getcwd())
+        output_file = folder_name + ".pdf"
+
+    pdf_files = [f for f in os.listdir(input_folder) if f.lower().endswith(".pdf")]
+    pdf_files.sort()  # Trie les fichiers par ordre alphabétique
+
+    if not pdf_files:
+        print(f"Aucun fichier PDF trouvé dans le dossier {input_folder}")
+        return
+
+    merged_pdf = fitz.open()
+
+    for pdf_file in pdf_files:
+        with fitz.open(os.path.join(input_folder, pdf_file)) as pdf:
+            merged_pdf.insert_pdf(pdf)
+
+    output_path = os.path.join(input_folder, output_file)
+    merged_pdf.save(output_path)
+    merged_pdf.close()
+
+    print(f"Les PDF ont été fusionnés dans {output_path}")
+
+
+def extract_pages(pdf_path):
+    """
+    Extrait toutes les pages d'un PDF dans des fichiers séparés.
+
+    :param pdf_path: Chemin du fichier PDF à traiter
+    """
+    pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
+    pdf_name = slugify(pdf_name)
+    output_folder = f"pages-{pdf_name}"
+    os.makedirs(output_folder, exist_ok=True)
+
+    with fitz.open(pdf_path) as pdf:
+        for page_num in range(len(pdf)):
+            output_pdf = fitz.open()
+            output_pdf.insert_pdf(pdf, from_page=page_num, to_page=page_num)
+            output_file = os.path.join(
+                output_folder, f"{pdf_name}-{page_num+1:02d}.pdf"
+            )
+            output_pdf.save(output_file)
+            output_pdf.close()
+
+    print(f"Les pages ont été extraites dans le dossier {output_folder}")
