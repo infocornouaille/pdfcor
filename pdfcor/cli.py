4500edf1e6ba9f67909f0515404835c6cafd84aa
infocornouaille <90894019+infocornouaille@users.noreply.github.com>
2024-10-13 08:16:57 +0200

    Update cli.py

--- pdfcor/cli.py
+++ pdfcor/cli.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from .core import process_folder
+from .core import process_folder, merge_pdfs, extract_pages
 
 
 def main():
@@ -24,15 +24,33 @@ def main():
         action="store_true",
         help="Redimensionner les images pour tenir sur une page A4",
     )
+    parser.add_argument(
+        "--fusion",
+        action="store_true",
+        help="Fusionner tous les PDF du dossier d'entrée",
+    )
+    parser.add_argument(
+        "--pages",
+        help="Extraire les pages du PDF spécifié",
+    )
 
     args = parser.parse_args()
 
     input_folder = os.path.abspath(args.input_folder)
-    output_folder = (
-        os.path.abspath(args.output_folder) if args.output_folder else input_folder
-    )
 
-    process_folder(input_folder, output_folder, args.recursive, args.resize)
+    if args.fusion:
+        merge_pdfs(args.input_folder)
+    elif args.pages:
+        extract_pages(args.pages)
+
+    else:
+
+        input_folder = os.path.abspath(args.input_folder)
+        output_folder = (
+            os.path.abspath(args.output_folder) if args.output_folder else input_folder
+        )
+
+        process_folder(input_folder, output_folder, args.recursive, args.resize)
 
 
 if __name__ == "__main__":
